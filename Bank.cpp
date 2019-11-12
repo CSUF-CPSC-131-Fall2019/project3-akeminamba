25da3dfa53cb0ac4e55e4c99b10c94f6408a667a
Your Name <you@example.com>
2019-11-11 22:12:59 -0800

    working on project 2

--- Bank.cpp
+++ Bank.cpp
@@ -6,3 +6,13 @@
 ** Member function definitions
 ************************************************/
 // PLEASE ADD IMPLEMENTATIONS OF ALL FUNCTION MEMBERS OF CLASS BANK
+
+Bank::Bank(const std::string & name,
+			const double & dues,
+			const unsigned int & month,
+			const unsigned int & day)
+{
+  //name_ = name;
+  //starting_ = starting;
+  //salary_ = salary;
+}
--- Bill.cpp
+++ Bill.cpp
@@ -8,20 +8,30 @@
 
 // ADD IMPLEMENTATION OF CONSTRUCTOR FOR CLASS Bill
 
-bool Bill::isOverdue(const unsigned int &currMonth, 
+Bill::Bill(const std::string & name,
+			const double & dues,
+			const unsigned int & month,
+			const unsigned int & day) {
+
+}
+
+bool Bill::isOverdue(const unsigned int &currMonth,
 		     const unsigned int& currDay ) {
-  // returns true if the due date for the bill is past 
+  // returns true if the due date for the bill is past
   // the date (month and day) given as parameters
   // false otherwise
 
 // TO BE COMPLETED
-
+if(currMonth == due_month_ && currDay > due_day_) {
+	return true;
+}
+	return false;
 }
 
 // This function member implementation is complete
-unsigned int Bill::daysOverdue(const unsigned int &currMonth, 
+unsigned int Bill::daysOverdue(const unsigned int &currMonth,
 			       const unsigned int& currDay ) {
-  // computes the number of days elapsed between the due date 
+  // computes the number of days elapsed between the due date
   // of the bill and the date (month and day) given as parameters
   if (due_month_ > currMonth )
     return 0;
@@ -58,6 +68,6 @@ unsigned int Bill::daysOverdue(const unsigned int &currMonth,
     tempMonth++;
   }
   counter = counter + currDay;
-  
+
   return counter;
 }
